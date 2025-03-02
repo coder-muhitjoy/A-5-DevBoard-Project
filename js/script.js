7c4bd4d8e74b81f5b7abd6d77eebc684f837aeea
M A Muhit Joy <muhitjoy2004@gmail.com>
2025-03-02 13:00:32 +0600

    js section added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ document.getElementById("card-discover").addEventListener('click', function(even
     window.location.href = "main.html"   
 
 })
+
+document.getElementById("")
     const btn = document.querySelectorAll(".btn");
     let sitCount = 6;
-    let addSitCount = 23;
+    let addSitCount = 20;
     for(let i = 0; i < btn.length; i++){
         const button = btn[i];
         button.addEventListener("click", function(){
@@ -24,18 +26,19 @@ document.getElementById("card-discover").addEventListener('click', function(even
                 sitItem2.innerText = addSitCount
             }
             if(sitItem){
-                alert("Board Update Successfully")  
-                
+                alert("Board Update Successfully")       
+            }   
+
+            const goodAlert = document.getElementById("task-assigned").innerText;
+            if(goodAlert == 0){
+                alert("congrats!!! you have completed all the current task")
             }
-            function getButtonDeskID() {
-                console.log("Hello! Clear History button clicked.");
-            }
-            
-            
+           
             button.disabled = true;
+
+
             const myDiv = document.getElementById("myDiv");
-            const p = document.createElement("p");
-            
+            const p = document.createElement("p");    
             p.textContent = "You have Complete The Task Optimize Home page at 12:10:06 AM";
             p.classList.add("p-4", "bg-gray-100", "rounded-lg", "font-bold", "text-[14px]", "mt-2");
             myDiv.appendChild(p);
@@ -56,5 +59,27 @@ document.getElementById("card-discover").addEventListener('click', function(even
         
         document.body.style.backgroundColor = randomColor;
       }
+
+    const dates2 = new Date();
+      const months = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+      const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const dates = dates2.getDate();
+    const month = months[dates2.getMonth()];
+    const year = dates2.getFullYear();
+
+    const setDate = dates + " " + month + " " + year;
+
+    const dayTo = days[dates2.getDay()];
+    const fixedDay = dayTo.slice(0, 3);
+
+    document.getElementById('day-name').innerText = fixedDay + ',';
+
+    document.getElementById('date-name').innerText = setDate;
+
+    
+
+     
 
      
